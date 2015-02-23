ca145ad344caa211278bfb96a940bcc972ab1284
Mykhailo Lakhmaniuk <lmiha@nm.ru>
2015-02-24 00:11:34 +0200

    second lesson

--- app/src/main/java/ua/studyjamck/lakhmaniuk/sunshine/MainActivity.java
+++ app/src/main/java/ua/studyjamck/lakhmaniuk/sunshine/MainActivity.java
@@ -1,19 +1,9 @@
 package ua.studyjamck.lakhmaniuk.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,41 +42,6 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        private  ArrayAdapter<String>weekListAdapter;
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            String[] weekDays = new String[]
-                    {"Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 70/46",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Rainy - 64/51",
-                    "Fri - Foggy - 70/46",
-                    "Sat - Sunny - 76/68"};
-
-            List<String> weekDaysArrList = new ArrayList<String>();
-            weekDaysArrList.addAll(Arrays.asList(weekDays));
-
-            weekListAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textView,
-                    weekDaysArrList);
-
-            ListView listView = (ListView)rootView.findViewById(R.id.listView_forecast);
-            listView.setAdapter(weekListAdapter);
-
-            return rootView;
-        }
-    }
 }
